6c3c303cb794bf48fa092078fa82ed1f008eba3f
leeken <genwocaigudian@gmail.com>
2017-08-30 00:06:55 +0800

    step10_xhprof

--- index.php
+++ index.php
@@ -1,4 +1,5 @@
 <?php
+//xhprof_enable();
 try {
     define('APPLICATION_PATH', dirname(__FILE__));
 
@@ -11,4 +12,14 @@ try {
         'errmsg' => 'error' . $e->getMessage(),
     ]);
 }
+
+//$xhprof_data = xhprof_disable();
+//$XHPROF_ROOT = dirname(__FILE__) . '/application/library/ThirdParty/xhprof';
+//
+//include_once $XHPROF_ROOT . "/xhprof_lib/utils/xhprof_lib.php";
+//include_once $XHPROF_ROOT . "/xhprof_lib/utils/xhprof_runs.php";
+//$xhprof_runs = new XHProfRuns_Default();
+//
+//$run_id = $xhprof_runs->save_run($xhprof_data, "xhprof_foo");
+//header("XhprofID:" . $run_id);
 ?>
