022b35ece6ea8e4d9310afb4389f498784b2f999
leeken <genwocaigudian@gmail.com>
2017-09-06 23:47:46 +0800

    bui

---
+++ application/controllers/Admin.php
@@ -0,0 +1,22 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: likun
+ * Date: 2017/9/6
+ * Time: 22:29
+ */
+
+class AdminController extends Yaf_Controller_Abstract {
+
+    public function indexAction() {
+        return true;
+    }
+
+    public function loginAction() {
+        return true;
+    }
+
+    public function welcomeAction() {
+        return true;
+    }
+}
